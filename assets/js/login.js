d0fd1e60074e4ac11ab6a7069d0320152718c1b3
XjDGitHub <1291514712@qq.com>
2022-11-14 15:50:48 +0800

    添加注册post事件

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,19 +8,34 @@ $(function () {
         $('#link_reg').hide()
         $('#link_login').show()
     })
-    //自定义表单检验规则
+    // 从layui获取对象
     let form = layui.form
+    let layer = layui.layer
+    //自定义表单检验规则
+
     form.verify({
         pwd: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
         ],
         repwd: function (value) {
-            let pwd = $('#link_reg [name=password]').val()
+            let pwd = $('#link_reg [name=userpassword]').val()
             if (value !== pwd) {
                 return '两次密码输入不一致'
             }
         }
     }
     )
+    // 提交post事件
+    $('#reg_form').on('submit', function (e) {
+        e.preventDefault()
+        $.post('http://www.liulongbin.top:3007/api/reguser', { username: $("#reg_form [name=username]").val(), password: $("#reg_form [name=userpassword]").val() }, function (res) {
+            if (res.ststus !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功')
+            $('#reg_form a').click()
+            $('#reg_form').reset()
+        })
+    })
 })
